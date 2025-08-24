604b133c230440a24fe1840e8e0151657bbc66be
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-24 14:40:50 -0300

    feat(utils) add Category measures method

--- backend/src/utils/bmi.go
+++ backend/src/utils/bmi.go
@@ -11,3 +11,28 @@ func (measures *Measures) BMI() float64 {
 	bmi := measures.Weigth / (measures.Heigth * measures.Heigth)
 	return math.Round(bmi*100) / 100
 }
+
+func (measures *Measures) Category() string {
+	bmi := measures.BMI()
+
+	switch {
+	case bmi < 16:
+		return "Severe thinness ⚠️ 🪦"
+	case bmi >= 16 && bmi < 17:
+		return "Modarate thinness ⚠️ 🏃‍♂️"
+	case bmi >= 17 && bmi < 18.5:
+		return "Mild thinness ⚠️ 🍵"
+	case bmi >= 18.5 && bmi < 25:
+		return "Normal ✅ 🟢"
+	case bmi >= 25 && bmi < 30:
+		return "Overweight ⚠️ 🍔"
+	case bmi >= 30 && bmi < 35:
+		return "Obese class I 🔴 ⚠️"
+	case bmi >= 35 && bmi < 40:
+		return "Obese class II 🔴🚨"
+	case bmi >= 40:
+		return "Obese class III 🛑 🏥"
+	default:
+		return ""
+	}
+}
