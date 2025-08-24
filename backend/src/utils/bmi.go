b05aafc349c0b01a13ad5548227ac5abfcbab816
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-24 10:15:03 -0300

    feat(utils) add BMI measures method

--- backend/src/utils/bmi.go
+++ backend/src/utils/bmi.go
@@ -1,6 +1,13 @@
 package utils
 
+import "math"
+
 type Measures struct {
 	Weigth float64
 	Heigth float64
+}
+
+func (measures *Measures) BMI() float64 {
+	bmi := measures.Weigth / (measures.Heigth * measures.Heigth)
+	return math.Round(bmi*100) / 100
 }
