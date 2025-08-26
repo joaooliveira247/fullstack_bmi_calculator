e234e84967862bb7c8e03856916cce897bb2840c
joaooliveira247 <joaooliveira247@gmail.com>
2025-08-25 22:24:29 -0300

    feat(controllers): add GetBMI controller for POST /bmi/ route

--- backend/src/controllers/bmi.go
+++ backend/src/controllers/bmi.go
@@ -1,7 +1,31 @@
 package controllers
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joaooliveira247/fullstack_bmi_calculator/backend/src/schemas"
+	"github.com/joaooliveira247/fullstack_bmi_calculator/backend/src/utils"
+)
+
 type BMIController struct{}
 
 func NewBMIController() *BMIController {
 	return &BMIController{}
+}
+
+func (ctrl *BMIController) GetBMI(ctx *gin.Context) {
+	var bmi schemas.BMIIn
+
+	if err := ctx.ShouldBindJSON(&bmi); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	measures := utils.Measures(bmi)
+
+	ctx.JSON(
+		http.StatusOK,
+		&schemas.BMIOut{BMI: measures.BMI(), Message: measures.Category()},
+	)
 }
