c63050c52ac53a2fa08ce7a5aec3232ae934f2c4
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-11 20:57:39 -0300

    feat(components): add Card component

--- frontend/src/components/Cards.tsx
+++ frontend/src/components/Cards.tsx
@@ -3,3 +3,9 @@ interface CardProps {
     children: React.ReactNode;
 }
 
+const Card = (props: CardProps) => {
+    const cardProps = `bg-[#2D3249] w-full h-[${props.height}px] rounded-2xl`;
+    return <div className={cardProps}>{props.children}</div>;
+};
+
+export default Card;
