02a7b6fcb6aa1664d7eeca0ec0320083b0605dd3
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-15 15:45:27 -0300

    refactor(components/Cards): change Card component tailwind class that broken layout

--- frontend/src/components/Cards.tsx
+++ frontend/src/components/Cards.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 const Card = ({ height, children }: CardProps) => {
     return (
         <div
-            className="bg-[#2D3249] w-full rounded-2xl flex justify-center items-center"
+            className="bg-[#2D3249] w-full rounded-2xl flex flex-col items-center justify-center gap-2"
             style={{ height }}
         >
             {children}
