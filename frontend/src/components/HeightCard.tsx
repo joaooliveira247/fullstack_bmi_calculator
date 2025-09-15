b40271e1a1704191e5cbfadbab8e937181e45732
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-15 16:45:34 -0300

    feat(components/HeightCard): add HeightCard component and HeightCardProps interface

---
+++ frontend/src/components/HeightCard.tsx
@@ -0,0 +1,34 @@
+import Card from "./Cards";
+
+interface HeightCardProps {
+    value: number;
+    min?: number;
+    max?: number;
+    onChange: (newValue: number) => void;
+}
+
+const HeightCard = ({
+    value,
+    min = 50,
+    max = 260,
+    onChange,
+}: HeightCardProps) => {
+    return (
+        <Card height={189}>
+            <h2 className="text-2xl text-white">Heigth</h2>
+            <div className="text-white">
+                <span className="text-5xl">{value}</span>
+                <span>cm</span>
+            </div>
+            <input
+                type="range"
+                min={min}
+                max={max}
+                className="h-[2px] w-[270px] accent-[#E83D67]"
+                onChange={(e) => onChange(Number(e.target.value))}
+            />
+        </Card>
+    );
+};
+
+export default HeightCard;
