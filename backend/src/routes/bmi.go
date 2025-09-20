c76befd2f3c551a8bdec8289cbf6a931d86fa51c
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-20 18:56:30 -0300

    feat(routes): add CORSin BMIGroup router

--- backend/src/routes/bmi.go
+++ backend/src/routes/bmi.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joaooliveira247/fullstack_bmi_calculator/backend/src/controllers"
 )
@@ -9,6 +10,16 @@ func BMIRoutes(eng *gin.Engine) {
 	controller := controllers.NewBMIController()
 
 	bmiGroup := eng.Group("/bmi")
+
+	bmiGroup.Use(cors.New(
+		cors.Config{
+			AllowOrigins:     []string{"http://localhost:3000"},
+			AllowMethods:     []string{"POST"},
+			AllowHeaders:     []string{"Origin", "Content-Type"},
+			AllowCredentials: true,
+		},
+	))
+
 	{
 		bmiGroup.POST("/", controller.GetBMI)
 	}
