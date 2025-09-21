7ce02b31cff73ea9331ef58f0b2f87a65e995165
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-21 07:45:45 -0300

    feat(widgets/Home): add HomeProps interface

--- frontend/src/widgets/Home.tsx
+++ frontend/src/widgets/Home.tsx
@@ -1,3 +1,13 @@
 import { Dispatch, SetStateAction } from "react";
 
 type StateSetter = Dispatch<SetStateAction<number>>;
+
+interface HomeProps {
+    height: number;
+    setHeight: StateSetter;
+    weight: number;
+    setWeight: StateSetter;
+    age: number;
+    setAge: StateSetter;
+    onClickButton: () => void;
+}
