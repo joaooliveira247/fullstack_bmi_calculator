edc7cea3fcf9654753848f1091ecdd4d930b0ab9
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-21 16:26:42 -0300

    feat(app/page): add Home in main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+"use client";
+import { getBMI } from "@/services/bmiApi";
+import Home from "@/widgets/Home";
+import { useState } from "react";
+
+export default function Page() {
+    const [height, setHeight] = useState(152);
+    const [weight, setWeight] = useState(60);
+    const [age, setAge] = useState(26);
+
+    const handleCalculate = async () => {
+        try {
+            const result = await getBMI(weight, height);
+            console.log(result);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
-        <div>
-            <div></div>
-        </div>
+        <Home
+            height={height}
+            setHeight={setHeight}
+            weight={weight}
+            setWeight={setWeight}
+            age={age}
+            setAge={setAge}
+            onClickButton={handleCalculate}
+        />
     );
 }
