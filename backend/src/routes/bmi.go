699936c8be9ef9140a231a6c90dae285238e0180
joaooliveira247 <joaooliveira247@gmail.com>
2025-09-23 11:17:07 -0300

    refactor(routes/bmi): remove cors logic from routes

--- backend/src/routes/bmi.go
+++ backend/src/routes/bmi.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joaooliveira247/fullstack_bmi_calculator/backend/src/controllers"
 )
@@ -10,15 +9,6 @@ func BMIRoutes(eng *gin.Engine) {
 	controller := controllers.NewBMIController()
 
 	bmiGroup := eng.Group("/bmi")
-
-	bmiGroup.Use(cors.New(
-		cors.Config{
-			AllowOrigins:     []string{"http://localhost:3000"},
-			AllowMethods:     []string{"POST"},
-			AllowHeaders:     []string{"Origin", "Content-Type"},
-			AllowCredentials: true,
-		},
-	))
 
 	{
 		bmiGroup.POST("/", controller.GetBMI)
